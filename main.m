fa331f57db7df46123655b868ebf8d0dcb1ee850
Shr1ftyy <syeamtechdemon@gmail.com>
2022-04-25 16:22:23 +0930

    compute and basic graphics done

--- main.m
+++ main.m
@@ -7,19 +7,24 @@
 % NBodies = input("How many bodies would you like to run a simulation of (2 or more)?: \n");
 % assert(NBodies >= 2);
 % loadedVecs = loadVecs(NBodies);
-loadedVecs = [1 1 1 10000; 2 2 2 20000 ; 4 5 6 20000];
+% loadedVecs = [1 1 1 10000000; 2 2 2 20000000 ; 4 5 6 20000000];
 masses = loadedVecs(1:end, 4);
 posMat = loadedVecs(1:end, 1:3);
 % initial velocity and acceleration are zero for all particles
 velocityMat = zeros(size(posMat));
 accelMat = zeros(size(posMat));
+  init = posMat;
 t = 0; % intiial time in seconds 
-dt=1; % change in t per timestep
+dt=864000; % change in t per timestep
+
+fh = figure();
+view = scatter3(posMat(1:end, 1),posMat(1:end, 2),posMat(1:end, 3));
 
 while true
-  init = posMat;
   [posMat, velocityMat, accelMat] = updateSimGivenLast(posMat, velocityMat, accelMat, masses, dt); 
   disp(posMat);
+  set(view, 'XData', posMat(1:end, 1), 'YData', posMat(1:end, 2), 'ZData', posMat(1:end, 3)); 
+  drawnow();
 end
 
 
